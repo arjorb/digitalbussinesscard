5d072b6f1ce777a13a56fc1618ac4c5fe0f64f44
John U <arjorb@gmail.com>
2023-02-03 15:27:33 +0200

    done working on the info component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Info from './components/Info';
 function App() {
-  return <div className='w-64 h-full bg-gray-900 pb-5 rounded-lg'></div>;
+  return (
+    <div className=' w-68 h-full bg-gray-900 pb-5 rounded-lg'>
+      <Info />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <>
+      <div>
+        <img src='./Rectangle.png' alt='Profile Picture' />
+        <div className='text-center py-4'>
+          <h1 className='text-2xl font-bold tracking-wider'>Laura Smith</h1>
+          <p className='text-md font-normal text-[#F3BF99]'>Frontend Developer</p>
+          <span className='text-sm font-normal'>laurasmith.website</span>
+        </div>
+
+        <div className='flex justify-evenly my-2'>
+          <div className='bg-white py-2 px-5 w-max flex gap-1 rounded-sm'>
+            <img src='./message.svg' alt='email' />
+            <p className='text-black'>Email</p>
+          </div>
+          <div className='bg-blue-300 py-2 px-5 w-max flex gap-1 rounded-sm'>
+            <img src='./linkedin.svg' alt='linkedin' />
+            <p className='text-white'>LinkedIn</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Info;
